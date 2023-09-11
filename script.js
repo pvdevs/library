3c1ebbfd20085852463af4a111cd99ec55e4ffff
Paulo Victor <paulovictor.devs@gmail.com>
2023-09-11 01:01:14 -0300

    add basic code logic

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
     this.info = function () {
         console.log(`${title} by ${author}, ${pages} Pages.`);
-    }
+    }    
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 
 theHobbit.info;
+
+// Notes for myself -> I need to create the function that toggles if i read or not the book.
