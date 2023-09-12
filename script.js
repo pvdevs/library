0cac617274cde3bc0ca2d0e27cf6b3a06d581514
Paulo Victor <paulovictor.devs@gmail.com>
2023-09-12 18:41:36 -0300

    add logic to update display with new books, add logic to clear inputs after submit

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const bookAuthor = document.querySelector('#book-author');
 const bookPages = document.querySelector('#book-pages');
 const bookRead = document.querySelector('#book-read');
 const submitButton = document.querySelector('#modal-submit');
+const books = document.querySelector('.books');
 
 
 
@@ -16,8 +17,70 @@ addBook.addEventListener('click', (e) => {
 });
 
 submitButton.addEventListener('click', (e) => {
-
+    e.preventDefault()
+    const temp = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked);
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    bookPages.value = '';
+    bookRead.checked = false;
+    
+    //console.log(myLibrary); //remove later
+    displayBooks(temp.id - 1);
+    addBookModal.close();
 });
+
+bookRead.addEventListener('click', (e) => {
+    console.log(bookRead.checked);
+});
+
+
+function createBookCard(title, author, pages, read) {
+    const div = document.createElement('div');
+    div.classList.add('book');
+
+    const bookTitle = document.createElement('h2');
+    bookTitle.classList.add('book-info');
+    bookTitle.classList.add('title');
+
+    const bookAuthor = document.createElement('p');
+    bookAuthor.classList.add('book-info');
+    bookAuthor.classList.add('author');
+
+    const bookPages = document.createElement('p');
+    bookPages.classList.add('book-info');
+    bookPages.classList.add('pages');
+
+    const cardButtons = document.createElement('div'); // append os botoes aq // UNFINISHED (rever logica dos botoes de card)
+    cardButtons.classList.add('buttons');
+
+    const readButton = document.createElement('button');
+    readButton.classList.add('book-info');
+    readButton.classList.add('read');
+
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('book-info');
+    removeButton.classList.add('remove');
+
+    bookTitle.textContent = title;
+    bookAuthor.textContent = author;
+    bookPages.textContent = pages;
+    readButton.textContent = 'Read';
+    removeButton.textContent = 'Remove';
+
+    div.append(bookTitle, bookAuthor, bookPages);
+    div.appendChild(cardButtons);
+
+    cardButtons.append(readButton, removeButton);
+
+    books.appendChild(div);
+}
+
+function displayBooks(id) {
+    createBookCard(myLibrary[id].title, myLibrary[id].author, myLibrary[id].pages, myLibrary[id].read);
+}
+
+
+
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -36,7 +99,6 @@ const theHobbit1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 const theHobbit2 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 const theHobbit3 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 
-myLibrary.push(theHobbit1, theHobbit2, theHobbit3);
 
 Book.prototype.info = function () {
     console.log(`
@@ -47,8 +109,14 @@ Book.prototype.info = function () {
     ${this.id} Id.`);
 }
 
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
 theHobbit1.info();
 theHobbit2.info();
 theHobbit3.info();
+
+console.log(myLibrary);
 
 // Notes for myself -> I need to create the function that toggles if i read or not the book.
